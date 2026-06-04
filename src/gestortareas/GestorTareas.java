b29411d9ac588fd4c956645a185479b8091cc82d
ChrisColind <cristopher_rogelio@unitec.edu>
2026-06-04 15:17:52 -0600

    primer commit

---
+++ src/gestortareas/GestorTareas.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestortareas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rogelio
+ */
+public class GestorTareas{
+ 
+    public static void main(String[] args){
+        Scanner n = new Scanner(System.in);
+        int opcion = 0;
+ 
+        do{
+            System.out.println("\nGESTOR DE TAREAS");
+            System.out.println("=================");
+            System.out.println("1. Agregar tarea");
+            System.out.println("2. Mostrar tareas");
+            System.out.println("3. Completar tareas");
+            System.out.println("4. Salir");
+            System.out.print("Selecciona una opcion: ");
+ 
+            try{
+                opcion = n.nextInt();
+                n.nextLine();
+ 
+                if(opcion== 1){
+                    System.out.print("Ingresa la nueva tarea: ");
+                    String tarea = n.nextLine();
+                    Tareas.agregarTarea(tarea);
+ 
+                }else if(opcion == 2){
+                    Tareas.mostrarTareas();
+ 
+                }else if(opcion == 3){
+                    Tareas.mostrarTareas();
+                    System.out.print("Elija la tarea para completar: ");
+                    int numero = n.nextInt();
+                    n.nextLine();
+                    Tareas.completarTarea(numero);
+ 
+                }else if(opcion!=4){
+                    
+                }
+ 
+            }catch(Exception e){
+                System.out.println("Opcion no valida");
+                n.nextLine();
+                opcion = 0;
+            }
+ 
+        }while(opcion !=4);
+ 
+        n.close();
+    }
+}
---
+++ src/gestortareas/Tareas.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestortareas;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Rogelio
+ */
+public class Tareas{
+ 
+    static String archivo = "tareas.txt";
+ 
+    static ArrayList<String> leerTareas(){
+        ArrayList<String> lista = new ArrayList<>();
+ 
+        File a = new File(archivo);
+        if (!a.exists()){
+            return lista;
+        }
+ 
+        try{
+            FileReader Areader = new FileReader(archivo);
+            String linea = "";
+            int c;
+ 
+            while ((c = Areader.read()) != -1){
+                char car = (char) c;
+                if(car == '\n') {
+                    if (!linea.isEmpty()){
+                        lista.add(linea);
+                    }
+                    linea = "";
+                }else{
+                    linea+= car;
+                }
+            }
+ 
+            if (!linea.isEmpty()) {
+                lista.add(linea);
+            }
+ 
+            Areader.close();
+ 
+        } catch (Exception e) {
+            System.out.println("Error al leer el archivo.");
+        }
+ 
+        return lista;
+    }
+ 
+    static void guardarTareas(ArrayList<String> lista) {
+        try {
+            FileWriter fw = new FileWriter(archivo, false);
+ 
+            for (String tarea : lista) {
+                fw.write(tarea + "\n");
+            }
+ 
+            fw.close();
+ 
+        }catch(Exception e){
+            System.out.println("Error al guardar el archivo.");
+        }
+    }
+ 
+    static void agregarTarea(String nombre) {
+        ArrayList<String> lista = leerTareas();
+        lista.add("[ ] " + nombre);
+        guardarTareas(lista);
+        System.out.println("\nTarea agregada: " + nombre);
+    }
+ 
+    static void mostrarTareas() {
+        ArrayList<String> lista = leerTareas();
+ 
+        System.out.println("\nLISTA DE TAREAS");
+        System.out.println("====================");
+ 
+        if (lista.isEmpty()) {
+            System.out.println("No hay tareas todavia.");
+            return;
+        }
+ 
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println((i + 1) + ". " + lista.get(i));
+        }
+    }
+ 
+    static void completarTarea(int numero) {
+        ArrayList<String> lista = leerTareas();
+ 
+        if (numero < 1 || numero > lista.size()) {
+            System.out.println("Numero de tarea invalido.");
+            return;
+        }
+ 
+        String tarea = lista.get(numero - 1);
+ 
+        if (tarea.startsWith("[✓]")) {
+            System.out.println("Esa tarea ya estaba completada.");
+            return;
+        }
+ 
+        tarea = tarea.replace("[ ]", "[✓]");
+        lista.set(numero - 1, tarea);
+        guardarTareas(lista);
+ 
+        String nombre = tarea.replace("[x] ", "");
+        System.out.println("\nTarea #" + numero + " completada: " + nombre);
+    }
+}
